928cd328d720e35c85c05415e876519b48301a6e
Spencer Krum <nibz@cat.pdx.edu>
2012-12-13 17:31:30 -0800

    branchpoint.sh-16720

---
+++ branchpoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MASTER=master
+NUMBER=$RANDOm
+
+git log --oneline >  /tmp/branch-$NUMBER
+git log --oneline $MASTER > /tmp/master-$NUMBER
+
+BANCHPOINT=`comm -1 -2 /tmp/master-$NUMBER /tmp/branch-$NUMBER \
+  | head -n 1 \
+  | cut -d " " -f 1`
+
+echo $BRANCHPOINT
+
+rm /tmp/branch-$NUMBER
+rm /tmp/master-$NUMBER
+
+
+
+
